6ebb0fa230e34cc8bb5653472711f21d682dc9b9
Keerti Shetty <kes@ciklum.com>
2025-05-16 12:54:07 +0530

    testimonials section added in about us page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AboutUsHeroSection from "./section/AboutUsHeroSection";
 import CompanyInfoSection from "./section/CompanyInfoSection";
 import CompanyPartners from "./section/CompanyPartners";
+import TestimonialsSection from "./section/TestimonialsSection";
 
 const page = () => {
   return (
@@ -11,6 +12,9 @@ const page = () => {
       <div className="main-container-width global-padding">
         {/* <CompanyInfoSection /> */}
         <CompanyPartners/>
+      </div>
+      <div className="main-container-width global-padding mt-[75px]">
+        <TestimonialsSection/>
       </div>
     </div>
   );
