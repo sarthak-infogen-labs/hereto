5c0366d69cc751d43924e4d9913d67629f2b8b38
Krutarth-ciklum <kruh@ciklum.com>
2025-05-21 12:18:47 +0530

    split engineering offerings section into components

--- src/app/data-driven-transformation/[slug]/sections/EngineeringServicesSection.tsx
+++ src/app/data-driven-transformation/[slug]/sections/EngineeringServicesSection.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import EngineeringCard from "@/components/cards/EngineeringCard";
-import CustomDropdown from "@/components/shared/CustomDropdown/CustomDropdown";
-import React, { useState } from "react";
+import EngineeringSevicesComponent from "@/components/pages/data-driven-transformations/EngineeringSevicesComponent";
 
 const EngineeringServicesSection = () => {
-  const [hoveredContentIndex, setHoveredcontentIndex] = useState(0);
+
   const dataEngineeringOfferings = [
     {
       key: "ETL / ELT / Integration",
@@ -103,66 +101,10 @@ const EngineeringServicesSection = () => {
     },
   ];
 
-  const options = dataEngineeringOfferings.map((item, index) => ({
-    id: index.toString(),
-    label: item.key,
-    value: item.key,
-  }));
-
-  const selectedOption = options[hoveredContentIndex];
-  
   return (
-    <div className="mt-[92px] relative  w-full  flex flex-col items-center">
-      <div className="text-h1-semibold !font-semibold max-w-[666px] text-center text-[#EEE6FC">
-        We Provide End-to-End Data Engineering Offerings
-      </div>
-
-      <div
-        className="absolute blur-[70px] top-20 md:w-[851px] max-md:w-[602px] h-[222px] rounded-full left-1/2 -translate-x-1/2 z-[-1] "
-        style={{
-          background: `radial-gradient(50.98% 52.49% at 50.1% 34.98%, rgba(90, 50, 250, 0.3) 30%, rgba(26, 29, 62, 0.4) 100%)`,
-        }}
-      />
-
-      <div className=" flex flex-col  mt-8 px-4 md:hidden">
-        <CustomDropdown
-          options={options}
-          value={selectedOption?.value}
-          onChange={(selected) => {
-            const selectedIndex = options.findIndex(
-              (option) => option.value === selected
-            );
-            setHoveredcontentIndex(selectedIndex);
-          }}
-          label="Select a service"
-          placeholder="Choose a service"
-        />
-      </div>
-
-      <div className="max-md:mt-[24px] mt-[42px] lg:mt-[82px] w-full flex  ">
-        <div className="w-1/2 flex flex-col space-y-[54px] max-md:hidden ">
-          {dataEngineeringOfferings.map((item, index) => (
-            <div key={index} onMouseEnter={() => setHoveredcontentIndex(index)} className="cursor-pointer">
-              <li
-                className={`list-none text-h5 font-semibold ${
-                  hoveredContentIndex === index
-                    ? "text-[#EEE6FC]"
-                    : "text-[#B2A8BE]"
-                }`}
-              >
-                {item.key}{" "}
-              </li>
-            </div>
-          ))}
-        </div>
-
-        <div className="w-1/2 max-md:w-full  ">
-          <EngineeringCard
-            data={dataEngineeringOfferings[hoveredContentIndex].value}
-          />
-        </div>
-      </div>
-    </div>
+    <section>
+      <EngineeringSevicesComponent dataEngineeringOfferings={dataEngineeringOfferings}/> 
+    </section>
   );
 };
 
